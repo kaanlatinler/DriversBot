2b9a6cda48a9e2fc6618f5f0b6ac8d67472ce90b
Kaan Latinler <kaanlatinlerhd@gmail.com>
2024-08-27 16:54:15 +0300

    chore: Remove unused dependencies and configuration files

--- src/index.js
+++ src/index.js
@@ -2,27 +2,15 @@ require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
 const eventHandler = require('./handlers/eventHandler');
 
-const cors = require('cors');
-const express = require('express');
-const app = express();
-
-app.use(cors());
-
-app.get('/message', (req, res) => {
-  res.json({ message: 'Merhaba, bu bir Node.js ile döndürülen mesajdır!' });
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildPresences,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
-app.listen(8080, () => {
-  const client = new Client({
-    intents: [
-      IntentsBitField.Flags.Guilds,
-      IntentsBitField.Flags.GuildMembers,
-      IntentsBitField.Flags.GuildMessages,
-      IntentsBitField.Flags.GuildPresences,
-      IntentsBitField.Flags.MessageContent,
-    ],
-  });
-  
-  eventHandler(client);
-  client.login(process.env.BOT_TOKEN);
-});
+eventHandler(client);
+client.login(process.env.BOT_TOKEN);
